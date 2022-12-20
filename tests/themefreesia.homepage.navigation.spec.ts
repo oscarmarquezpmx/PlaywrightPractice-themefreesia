1b91facd68ce56e0ab27deadb81f166ef7460189
Oscar Marquez <41175516+oscarmarquezpmx@users.noreply.github.com>
2022-12-20 12:11:33 -0600

    update to latest version

--- tests/themefreesia.homepage.navigation.spec.ts
+++ tests/themefreesia.homepage.navigation.spec.ts
@@ -2,23 +2,33 @@ import { test, expect } from '@playwright/test';
 import { ThemefreesiaHome } from '../pages/themefreesia.home.page';
 import { HomePageHeader } from '../pages/themefreesia.homeheader.page';
 import { SearchPage } from '../pages/themefreesia.searchresults.page';
+import { ProductCategoryPage } from '../pages/themefreesia.productcategory.page';
+import { HomePageFooter } from "../pages/themefreesia.homefooter.page";
+
 
 
 test.describe(' Homepage Test', () => {
     let freesiaHome: ThemefreesiaHome;
     let homeheader: HomePageHeader;
+    let homePageFooter: HomePageFooter;
     let searchPage: SearchPage;
+    let productCategoryPage: ProductCategoryPage;
+
+
 
 
     test.beforeEach(async ({ page }) => {
         freesiaHome = new ThemefreesiaHome(page);
         homeheader = new HomePageHeader(page);
+        homePageFooter = new HomePageFooter(page);
         searchPage = new SearchPage(page);
+        productCategoryPage = new ProductCategoryPage(page);
 
 
         await page.goto("https://demo.themefreesia.com/shoppingcart/");
+        await expect(freesiaHome.homeLogoImage).toBeVisible();
 
-        await freesiaHome.getStarted();
+        //await freesiaHome.getStarted();
 
 
     });
@@ -37,15 +47,47 @@ test.describe(' Homepage Test', () => {
 
     test('should be able to navigate in the main Home menu options', async ({ page }) => {
         await expect(freesiaHome.homeLink).toBeVisible();
-        await freesiaHome.homeLink.click();
+        await freesiaHome.homeLink.hover();
         await expect(freesiaHome.megaLinkProductCategory).toBeVisible();
         await expect(freesiaHome.megaLinkCustomLinks).toBeVisible();
         await expect(freesiaHome.megaLinkFeaturedProduct).toBeVisible();
 
         // Product Category
         await expect(freesiaHome.megaMenuOptionUncategorized).toBeVisible();
+        await freesiaHome.megaMenuOptionUncategorized.click();
+        await expect(productCategoryPage.pageMessageNotFound).toBeVisible();
+        await expect(productCategoryPage.breadcrumb).toHaveText("Theme Freesia Sites > Shopping Cart > Products > Uncategorized");
+
+        //Furniture
+        await expect(freesiaHome.homeLink).toBeVisible();
+        await freesiaHome.homeLink.hover();
         await expect(freesiaHome.megaMenuFurniture).toBeVisible();
+        await freesiaHome.megaMenuFurniture.click();
+
+        let product = '80';
+        let text = "Raw Oak Shelf";
+        let price = "15.00"
+        let priceOriginal = "20.00";
+
+        let searchResult = await searchPage.addThumbnail(page, product, text, price, priceOriginal);
+        await expect(searchResult.thumbImage).toBeVisible();
+        await expect(searchResult.thumbText).toBeVisible();
+        await expect(searchResult.thumbPrice).toBeVisible();
+        await expect(searchResult.thumbPriceOriginal).toBeVisible();  //optional validation
+        await expect(searchResult.thumbAddToCart).toBeVisible();
+
+
+        // Glasses
+        await expect(freesiaHome.homeLink).toBeVisible();
+        await freesiaHome.homeLink.hover();
         await expect(freesiaHome.megaMenuGlasses).toBeVisible();
+        freesiaHome.megaMenuGlasses.click();
+        await expect(productCategoryPage.pageMessageNotFound).toBeVisible();
+        await expect(productCategoryPage.breadcrumb).toHaveText("Theme Freesia Sites > Shopping Cart > Products > Glasses");
+
+
+
+
     });
 
     test('should be able to search for the articles he wants to buy', async ({ page }) => {
@@ -53,7 +95,6 @@ test.describe(' Homepage Test', () => {
         let product = '82';
         let text = "Stone Bracelet";
         let price = "188.00"
-
         let searchResult = await searchPage.addThumbnail(page, product, text, price);
         await expect(searchResult.thumbImage).toBeVisible();
         await expect(searchResult.thumbText).toBeVisible();
@@ -65,13 +106,35 @@ test.describe(' Homepage Test', () => {
         let price2 = "1,900.00"
         let priceOriginal2 = "2,000.00";
 
-        let searchResult2 = await searchPage.addThumbnail(page, product2, text2, price, priceOriginal2,);
+        let searchResult2 = await searchPage.addThumbnail(page, product2, text2, price2, priceOriginal2);
         await expect(searchResult2.thumbImage).toBeVisible();
         await expect(searchResult2.thumbText).toBeVisible();
         await expect(searchResult2.thumbPrice).toBeVisible();
-        await expect(searchResult2.thumbPriceOriginal).toBeVisible();
+        await expect(searchResult2.thumbPriceOriginal).toBeVisible();  //optional validation
         await expect(searchResult2.thumbAddToCart).toBeVisible();
     });
 
+
+    test("The user should be able to see the footer with the payment options", async ({ page }) => {
+
+        let paymentAmex: string;
+        paymentAmex = "amex";
+
+        let paymentOption1 = await homePageFooter.getPaymentOption(page, paymentAmex);
+        await expect(paymentOption1.paymentOption).toBeVisible();
+
+        let paymentMasterCard: string;
+        paymentMasterCard = "mastercard";
+
+        let paymentOption2 = await homePageFooter.getPaymentOption(page, paymentMasterCard);
+        await expect(paymentOption2.paymentOption).toBeVisible();
+
+        let paymentVisa: string;
+        paymentVisa = "visa";
+
+        let paymentOption3 = await homePageFooter.getPaymentOption(page, paymentVisa);
+        await expect(paymentOption3.paymentOption).toBeVisible();
+
+    })
 
 });
